f815d265ec6503a6d1b663e6ac96ad016fd23ea0
Toni Andreev <111848210+TonniAndreev@users.noreply.github.com>
2025-06-12 02:35:06 +0300

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -2,10 +2,10 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname, {
-  // CSS is disabled for React Native
+  isCSSEnabled: false,
 });
 
-// Ensure we're targeting mobile platforms
-config.resolver.platforms = ['ios', 'android', 'native'];
+// Handle platform-specific extensions
+config.resolver.platforms = ['ios', 'android'];
 
 module.exports = config;
