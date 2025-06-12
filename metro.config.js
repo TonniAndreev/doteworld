cd1f9062a81bb780113a1cd77d6e0a661a19aeb0
Toni Andreev <111848210+TonniAndreev@users.noreply.github.com>
2025-06-12 03:05:06 +0300

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -3,9 +3,10 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname, {
   isCSSEnabled: false,
-});
-defaultConfig.resolver.sourceExts.push('cjs');
+  defaultConfig.resolver.sourceExts.push('cjs');
 defaultConfig.resolver.unstable_enablePackageExports = false;
+});
+
 // Handle platform-specific extensions
 config.resolver.platforms = ['ios', 'android'];
 
