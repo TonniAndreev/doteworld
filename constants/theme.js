04b991d2e847c44e10ae5bb4979a1ab8e8de1a4c
Ddhype <dungyov@agencyhype.com>
2025-06-27 15:50:22 +0300

    Updated theme.js

--- constants/theme.js
+++ constants/theme.js
@@ -39,7 +39,7 @@ export const COLORS = {
   warningDark: '#D17D00',
   
   error: '#FF3B30',
-  errorLight: 'rgba(255, 59, 48, 0.2)',
+  errorLight: 'rgba(255, 59, 48, 0.5)',
   errorMedium: 'rgba(255, 59, 48, 0.3)',
   errorDark: '#D32F2F',
   
