e45daef1721763d6336e54b76cfd0a78f6e84d6b
Ddhype <dungyov@agencyhype.com>
2025-06-30 14:36:43 +0300

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -43,7 +43,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="leaderboard"
         options={{
-          title: 'Leaderboard',
+          title: 'Rankings',
           tabBarIcon: ({ color, size }) => (
             <Trophy size={size} color={color} />
           ),
