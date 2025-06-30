5faa7bfb6e9f4013631d2f6cc939dd895671f14d
Toni Andreev <111848210+TonniAndreev@users.noreply.github.com>
2025-06-30 20:52:18 +0300

    Fix Dog Markers Visibility on Map

--- components/map/DogMarker.tsx
+++ components/map/DogMarker.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { Marker } from 'react-native-maps';
 import UserAvatar from '@/components/common/UserAvatar';
 import { COLORS } from '@/constants/theme';
