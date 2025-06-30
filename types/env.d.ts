41102ab714f8d21f8bcbbe177381cc4e645fd046
Ddhype <dungyov@agencyhype.com>
2025-06-30 10:32:56 +0300

    Set up Supabase environment variables

--- types/env.d.ts
+++ types/env.d.ts
@@ -10,6 +10,8 @@ declare global {
       EXPO_PUBLIC_GOOGLE_MAPS_API_KEY: string;
       EXPO_PUBLIC_ADMOB_ANDROID_APP_ID: string;
       EXPO_PUBLIC_ADMOB_IOS_APP_ID: string;
+      EXPO_PUBLIC_SUPABASE_URL: string;
+      EXPO_PUBLIC_SUPABASE_ANON_KEY: string;
     }
   }
 }
